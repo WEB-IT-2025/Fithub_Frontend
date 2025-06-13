0814edf71396e568b1280ccdbff73e4b76bf0f6d
keyi1000 <keyi0351fps@gmail.com>
2025-06-13 11:35:02 +0900

    [update]indexのルーティングを返納するため場所と内容を変更

--- app/index.tsx
+++ app/index.tsx
@@ -1,60 +1,38 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 
-import { Image } from 'expo-image'
-import { useRouter } from 'expo-router'
-import { Link } from 'expo-router'
-import { Platform, StyleSheet } from 'react-native'
-import tw from 'tailwind-react-native-classnames'
 
-import { HelloWave } from '@/components/HelloWave'
-import ParallaxScrollView from '@/components/ParallaxScrollView'
-import { ThemedText } from '@/components/ThemedText'
-import { ThemedView } from '@/components/ThemedView'
-import Button from '@/components/common/Button'
 
+import { Image } from 'expo-image';
+import { useRouter } from 'expo-router';
+import { Link } from 'expo-router';
+import { Platform, StyleSheet } from 'react-native';
+import { View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+
+
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import Button from '@/components/common/Button';
+
+
+
+
+
+// ...existing code...
 export default function HomeScreen() {
-    // const router = useRouter()
-
-    // useEffect(() => {
-    //     router.replace('/login')
-    // }, [router])
-
-    // style={tw`p-4 bg-blue-500`}
     return (
-        <ParallaxScrollView
-            headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-            headerImage={
-                <Image
-                    source={require('@/assets/images/partial-react-logo.png')}
-                    style={styles.reactLogo}
-                />
-            }
-        >
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Link href='/(auth)/login'>
                 <ThemedText>ログイン画面へ</ThemedText>
-            </Link>{' '}
-            <Link href='/(tabs)/home'>
+            </Link>
+            <Link href='/(tabs)/home' style={tw`m-4`}>
                 <ThemedText>ホーム画面へ</ThemedText>
-            </Link>{' '}
-        </ParallaxScrollView>
+            </Link>
+        </View>
     )
 }
+// ...existing code...
 
-const styles = StyleSheet.create({
-    titleContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 8,
-    },
-    stepContainer: {
-        gap: 8,
-        marginBottom: 8,
-    },
-    reactLogo: {
-        height: 178,
-        width: 290,
-        bottom: 0,
-        left: 0,
-        position: 'absolute',
-    },
-})
