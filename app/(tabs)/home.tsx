858aadd1ebac1622d232302223b163cd4d96ee80
Yu-deeptale <wintinver@gmail.com>
2025-06-20 15:24:43 +0900

    Tab、Homeのボタンをfontawesome仕様に変更。readmeにインストール仕様記載

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View, TouchableOpacity, Modal } from 'react-native'
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faCalendar } from "@fortawesome/free-regular-svg-icons";
+import { faCoffee, faDog, faPerson, faScroll } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import TabBar from '../components/TabBar'
 import MissionBoard from '../components/missionboard'
+
+
+const iconStyle = { color: '#1DA1F2', fontSize: 32 };
 
 const HomeScreen = () => {
     const [modalVisible, setModalVisible] = useState(false)
@@ -9,12 +16,28 @@ const HomeScreen = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>ホーム</Text>
-            <TouchableOpacity
-                style={styles.missionButton}
-                onPress={() => setModalVisible(true)}
-            >
-                <Text style={styles.missionButtonText}>ミッションボード</Text>
-            </TouchableOpacity>
+            <View style={styles.buttonGroup}>
+                
+                <TouchableOpacity
+                    style={styles.circleButton}
+                    onPress={() => {/* プロフィール画面へ遷移など */}}
+                >
+                    <FontAwesomeIcon style={iconStyle} icon={faPerson} size={30}  color='black'/>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.circleButton}
+                    onPress={() => {/* ペット画面へ遷移など */}}
+                >
+                    <FontAwesomeIcon style={iconStyle} icon={faDog} size={30} color='black' />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.circleButton}
+                    onPress={() => setModalVisible(true)}
+                >
+                    <FontAwesomeIcon style={iconStyle} icon={faScroll} size={30} color='black' />
+                </TouchableOpacity>
+                
+            </View>
 
             <Modal
                 visible={modalVisible}
@@ -45,9 +68,10 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
         padding: 20,
+        paddingBottom: 120, // ← TabBarの高さ分だけ下に余白を追加
     },
     title: {
         fontSize: 24,
@@ -55,11 +79,14 @@ const styles = StyleSheet.create({
         marginBottom: 20,
     },
     missionButton: {
-        backgroundColor: '#4caf50',
-        paddingVertical: 12,
-        paddingHorizontal: 24,
-        borderRadius: 8,
+        backgroundColor: '#fff',
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        alignItems: 'center',
+        justifyContent: 'center',
         marginBottom: 20,
+        elevation: 2,
     },
     missionButtonText: {
         color: '#fff',
@@ -90,6 +117,21 @@ const styles = StyleSheet.create({
         right: 0,
         bottom: 0,
         backgroundColor: 'transparent',
+    },
+    buttonGroup: {
+        alignItems: 'flex-start',
+        width: '100%',
+        marginBottom: 20,
+    },
+    circleButton: {
+        backgroundColor: '#fff',
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 16,
+        elevation: 2,
     },
 })
 
